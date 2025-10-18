27ba7f72d5ede33f0bb1a8be5d86f4985c425213
ardianilyas <ardianilyas@gmail.com>
2025-10-19 00:10:26 +0700

    feat: update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { trpc } from "@/utils/trpc";
 export default function Home() {
   const { data: users, isLoading } = trpc.user.getAll.useQuery();
 
-  if(isLoading) return <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">Loading...</div>
+  if(isLoading) return <div className="font-sans text-center">Loading...</div>
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
