b38d28aaf6603ed9db26374926664921ac28ba87
ardianilyas <ardianilyas@gmail.com>
2025-10-19 00:09:19 +0700

    feat: add auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export async function middleware(request: NextRequest) {
+	const sessionCookie = getSessionCookie(request);
+
+	if (!sessionCookie) {
+		return NextResponse.redirect(new URL("/sign-in", request.url));
+	}
+
+    if(sessionCookie && request.nextUrl.pathname === "/sign-in" || request.nextUrl.pathname === "/sign-up") {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/", "/sign-in", "/sign-up"], // Specify the routes the middleware applies to
+};
