02122a84df3cc05e18fef8381847adac42b7ec39
ardianilyas <ardianilyas@gmail.com>
2025-10-20 19:47:30 +0700

    feat: add isAdmin middleware and adminProcedure

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,6 +1,7 @@
 import { initTRPC, TRPCError } from '@trpc/server'
 import superjson from 'superjson'
 import type { Context } from './context'
+import { Role } from '@/generated/prisma';
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
@@ -22,4 +23,17 @@ const isAuthed = t.middleware(({ ctx, next }) => {
   });
 });
 
+const isAdmin = t.middleware(({ ctx, next }) => {
+  if(ctx.session?.user.role !== Role.ADMIN) {
+    throw new TRPCError({ code: "FORBIDDEN", message: "You don't have access to this route" });
+  };
+
+  return next({
+    ctx: {
+      user: ctx.session.user,
+    },
+  });
+});
+
 export const authProcedure = t.procedure.use(isAuthed);
+export const adminProcedure = authProcedure.use(isAdmin);
