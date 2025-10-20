d3929f879e7e4116919220a8bfd58fe294ec1558
ardianilyas <ardianilyas@gmail.com>
2025-10-20 18:20:39 +0700

    feat: update auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,9 +6,16 @@ type Props = {
 
 export default function AuthLayout({ children }: Props) {
     return (
-        <div className='flex items-center justify-center h-screen bg-neutral-100'>
-            <div className="max-w-lg bg-white rounded-md shadow-md p-6 w-full">
-                {children}
+        <div className='h-screen w-full'>
+            <div className="min-h-screen flex justify-between items-center">
+                <div className='basis-5/12 bg-violet-500 p-12 min-h-screen'>
+                    {/* Here the content soon */}
+                </div>
+                <div className="basis-7/12 bg-neutral-50 min-h-screen flex justify-center items-center">
+                    <div className="max-w-md w-full">
+                        {children}
+                    </div>
+                </div>
             </div>
         </div>
     )
