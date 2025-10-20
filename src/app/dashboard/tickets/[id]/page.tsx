aa376e97480b416b577d885c230f94d1e0d647de
ardianilyas <ardianilyas@gmail.com>
2025-10-20 19:46:23 +0700

    feat: add ticket detail page

---
+++ src/app/dashboard/tickets/[id]/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { trpc } from '@/utils/trpc';
+import { useParams, useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
+import { toast } from 'sonner';
+
+export default function TicketDetailPage() {
+    const params = useParams<{ id: string }>();
+    const ticketId = params.id;
+    const router = useRouter();
+
+    const { data: ticket, isLoading, error } = trpc.ticket.getTicket.useQuery(
+        { id: ticketId },
+        { enabled: !!ticketId }
+    )
+
+    useEffect(() => {
+        if (error?.data?.code === "FORBIDDEN") {
+          // ✅ UX smooth: redirect pelan ke dashboard
+          const timeout = setTimeout(() => {
+            toast.error(error.message);
+            router.replace("/dashboard/tickets");
+          }, 1000);
+    
+          return () => clearTimeout(timeout);
+        }
+    }, [error, router]);
+
+    return (
+        <>
+            <div>
+                <h1 className='text-2xl text-neutral-800 font-medium'>Ticket Detail</h1>
+                <p className='text-neutral-600 text-sm leading-relaxed'>Solved the ticket as soon as possible</p>
+            </div>
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : ticket ? (
+                <div className='p-6 shadow-md rounded-md max-w-xl w-full'>
+                    <h1 className='text-2xl text-neutral-800 font-medium'>{ticket.title}</h1>
+                    <p className='text-neutral-600 text-sm leading-relaxed'>{ticket.description}</p>
+                </div>
+            ) : (
+                <div>
+                    <h1 className='text-2xl text-neutral-800 font-medium'>Ticket not found</h1>
+                </div>
+            )}
+        </>
+    )
+}
