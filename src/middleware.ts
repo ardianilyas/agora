111272cd92a238da0d3b16c4bdcaa4d8c7f0e738
ardianilyas <ardianilyas@gmail.com>
2025-10-20 22:26:09 +0700

    feat: update middleware to check admin route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,39 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSessionCookie } from "better-auth/cookies";
+import { auth } from "./lib/auth";
+import { Role } from "./generated/prisma";
+import { betterFetch } from "@better-fetch/fetch";
+
+type Session = typeof auth.$Infer.Session;
 
 export async function middleware(request: NextRequest) {
-  const sessionCookie = getSessionCookie(request);
   const { pathname } = request.nextUrl;
+  const { data: session } = await betterFetch<Session>("/api/auth/get-session", {
+		baseURL: request.nextUrl.origin,
+		headers: {
+			cookie: request.headers.get("cookie") || "", // Forward the cookies from the request
+		},
+	});
 
   // ✅ 1. Kalau belum login & akses halaman privat, redirect ke sign-in
   const isAuthPage = pathname === "/sign-in" || pathname === "/sign-up";
   const isProtectedPage = pathname.startsWith("/dashboard");
+  const isAdminPage = pathname.startsWith("/dashboard/admin");
 
-  if (!sessionCookie && isProtectedPage) {
+  if (!session && isProtectedPage) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   // ✅ 2. Kalau sudah login & coba buka sign-in / sign-up, redirect ke home
-  if (sessionCookie && isAuthPage) {
+  if (session && isAuthPage) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if(session && isAdminPage) {
+    const role = session?.user.role;
+
+    if (role !== Role.ADMIN) {
+      return NextResponse.redirect(new URL("/403", request.url));
+    }
   }
 
   // ✅ 3. Kalau tidak ada kondisi di atas, lanjutkan normal
