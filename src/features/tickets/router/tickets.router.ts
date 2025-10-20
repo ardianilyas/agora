0f037549a56eaf67613cd472d6741f9d5905b889
ardianilyas <ardianilyas@gmail.com>
2025-10-20 19:45:47 +0700

    feat: add get ticket router

--- src/features/tickets/router/tickets.router.ts
+++ src/features/tickets/router/tickets.router.ts
@@ -1,15 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { authProcedure, router } from "@/server/trpc/trpc";
-import { createTicketSchema } from "../schema/ticket.schema";
-import { createTicket } from "../service/ticket.service";
+import { createTicketSchema, getTicketSchema } from "../schema/ticket.schema";
+import { createTicket, getTicket } from "../service/ticket.service";
+import { TRPCError } from "@trpc/server";
 
 export const ticketRouter = router({
     getTickets: authProcedure.query(async ({ ctx }) => {
         return await ctx.prisma.ticket.findMany({ 
-            where: { requesterId: ctx.session?.user.id },
+            where: { requesterId: ctx.user.id },
             orderBy: { createdAt: "desc" }, 
         });
     }),
     createTicket: authProcedure.input(createTicketSchema).mutation(async ({ ctx, input }) => {
         return await createTicket(ctx, input);
-    })
+    }),
+    getTicket: authProcedure.input(getTicketSchema).query(async ({ ctx, input }) => {
+        try {
+            const ticket = await getTicket(input.id, ctx);
+            return ticket;
+        } catch (error: any) {
+            throw new TRPCError({
+                code: 'FORBIDDEN',
+                message: error.message
+            });
+        };
+    }),
 })
